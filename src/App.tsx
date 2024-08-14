49d25e555c03a4175770944ba2e4ac0dd805b360
lucasvegalv <lucasvega02@gmail.com>
2024-08-14 16:52:05 -0300

    Fix style bug

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Results from './ui/Results/Results';
 function App() {
   return (
     <div className=' bg-clr-neutral-100 h-screen flex justify-center items-center w-full'>
-      <main className='bg-white w-full h-screen md:h-4/5 md:w-3/4 flex flex-col md:flex-row jakarta-500 md:justify-center items-stretch'>
+      <main className='bg-white md:rounded-3xl w-full h-screen md:h-4/5 md:w-3/4 flex flex-col md:flex-row jakarta-500 md:justify-center items-stretch'>
         <Form />
         <Results />
       </main>
